32433cfc2f0f6f91a5d4c44157120f7e7eb71ed4
Luigi Talamona <luigi@talamona.org>
2018-01-25 16:09:52 +0100

    WIP

--- src/main/java/trz/gui/ThorwinDemo.java
+++ src/main/java/trz/gui/ThorwinDemo.java
@@ -31,22 +31,24 @@ public class ThorwinDemo extends Application {
 
         LineChart<Number,Number> chart =
                 new LineChart<Number, Number>(xAxis, yAxis);
-        chart.setCreateSymbols(false);
+        chart.setCreateSymbols(true);
         chart.getData().add(series1);
         chart.getData().add(series2);
 
         // setup chart series
-        double[] xs = {0.0, 0.5, 2.0, 3.0, 4.0, 5.0};
-        double[] ys = {0.0, 4.0, 3.0, 3.5, 3.0, 2.5};
+        double[] xs = this.getXData();
+        double[] ys = this.getYData();
+//        double[] xs = {0.0, 0.5, 2.0, 3.0, 4.0, 5.0};
+//        double[] ys = {0.0, 4.0, 3.0, 3.5, 3.0, 2.5};
 
         for (int i = 0; i < xs.length; i++) {
             series1.getData().add(new XYChart.Data<>(xs[i], ys[i]));
         }
 
         // calculate the polynomial coefficients and calculate trend points
-        double[] coefficients = polyfit(xs, ys, 4);
+        double[] coefficients = polyfit(xs, ys, 10);
 
-        for (double x = 0; x <= 5.0; x += 0.05) {
+        for (double x = 0; x <= 10.0; x += 0.05) {
             double y = polynomial(x, coefficients);
             series2.getData().add(new XYChart.Data<>(x,y));
         }
@@ -57,6 +59,21 @@ public class ThorwinDemo extends Application {
         primaryStage.setScene(scene);
         primaryStage.sizeToScene();
         primaryStage.show();
+    }
+    private double[] getXData(){
+        double[] retValue ={0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,
+                2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,
+                5.3,5.4,5.5,5.6,5.7,5.8,5.9,6.0,6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8,6.9,7.0,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8.0,8.1,
+                8.2,8.3,8.4,8.5,8.6,8.7,8.8,8.9,9.0,9.1,9.2,9.3,9.4,9.5,9.6,9.7,9.8,9.9};
+        return retValue;
+    }
+    private double[] getYData(){
+        double[] retValue = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+                0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,-0.3,-0.4,-0.7,-0.8,-1.1,-1.2,-1.6,-1.9,-2.0,-2.1,-2.2,-2.3,-2.4,-2.5,-2.6,-2.7,-2.2,
+                -2.2,-2.2,-2.2,-2.2,-2.2,-2.2,-1.8,-1.5,-1.2,-0.8,-0.5,-0.4,0.0,0.0,0.3,0.4,0.5,0.6,0.7,0.8,0.3,0.0,0.0,-0.3,-0.4,
+                -0.7,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.1,-1.1,-1.1,-1.1,-1.1,-1.1,-1.1,-1.1,
+                -1.1,-1.1};
+        return retValue;
     }
 
 
